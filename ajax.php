365081166cfa4b05a153e024ad02fa1b14d89047
Sean <conk3r_xd@hotmail.com>
2016-07-30 01:11:31 +0200

    all

---
+++ ajax.php
@@ -0,0 +1,16 @@
+
+	<?php
+
+// echo delete("DELETE.html");
+
+// $file = "DELETE.html";
+// if (!unlink($file))
+  // {
+  // echo ("Error deleting $file");
+  // }
+// else
+  // {
+  // echo ("Deleted $file");
+  // }
+
+	?>
